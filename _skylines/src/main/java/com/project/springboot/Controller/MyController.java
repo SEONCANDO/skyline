a574def1808c31c04b95537183cdbbbd5d5f459c
Fender <118142982+Cmguitars@users.noreply.github.com>
2023-02-15 18:57:34 +0900

    asd
    asd

--- _skylines/src/main/java/com/project/springboot/Controller/MyController.java
+++ _skylines/src/main/java/com/project/springboot/Controller/MyController.java
@@ -2,36 +2,20 @@ package com.project.springboot.Controller;
 
 import java.util.List;
 
-import javax.tools.DocumentationTool.Location;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.springboot.DAO.FlightScheduleDAO;
 import com.project.springboot.DAO.PlatformDAO;
 import com.project.springboot.DAO.ReservationDAO;
 import com.project.springboot.DAO.UserDAO;
-import com.project.springboot.Service.UserService;
 import com.project.springboot.Service.UserServiceImp;
-import com.project.springboot.VO.KakaoProfileVO;
-import com.project.springboot.VO.NaverprofileVO;
-import com.project.springboot.VO.OAuthTokenVO;
-import com.project.springboot.VO.PlatformVO;
 import com.project.springboot.VO.UserVO;
 
 import jakarta.servlet.http.HttpSession;
@@ -59,23 +43,14 @@ public class MyController {
 		this.rsMapper = rsMapper;
 		this.userMapper = userMapper;
 	}
-	
 
-	@GetMapping("/test")
-	public String list(Model model) {
-		List<PlatformVO> plst = pfMapper.selectName("i");
-		model.addAttribute("list",plst);
-		return "testlist";
-	}
-	
-
-	@GetMapping("/test2")
-	public void list2() {
-	}
 
 	@GetMapping("/login")
 	public void login() {
 	}
+	
+	
+	
 	
 	@PostMapping("/login")
 	public String callbackSkyline(UserVO user,Model mo, HttpSession session) {
@@ -110,8 +85,38 @@ public class MyController {
 		return "/alert";
 	}
 	
+	@GetMapping("/signup")
+	public void signup() {
+	}
+	
+	@PostMapping("/inspection")
+	public @ResponseBody boolean inspectionId(@RequestParam("userId") String userId) {
+		System.out.println(userId);
+		UserVO user_db = userMapper.inspectionId(userId);
+		
+		if(user_db == null) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	@PostMapping("/signup")
+	public String signupSkyline(UserVO user,Model mo,HttpSession session) {
+		int i = userMapper.fullSign(user);
+		mo.addAttribute("alert","회원가입이 완료되었습니다.");
+		mo.addAttribute("url","/home");
+		session.setAttribute("user", user);
+		return "/alert";
+	}
+	
+	
 	@GetMapping("/home")
 	public void home() {
+	}
+	
+	@GetMapping("/mypage")
+	public void mypage() {
 	}
 	
 	@GetMapping("/auth/callback/{type}")
@@ -132,7 +137,7 @@ public class MyController {
 		 * System.out.println("항공예매사이트 유저Id>>"+user.getUserId());
 		 * System.out.println("항공예매사이트 유저Email>>"+user.getUserEmail());
 		 */
-		UserVO userId_DB = userMapper.selectOneId(user.getUserId());
+		UserVO userId_DB = userMapper.inspectionId(user.getUserId());
 		if(userId_DB != null) {
 			System.out.println(user);
 			if(userId_DB.getUserFirstName() != null) {
@@ -154,13 +159,5 @@ public class MyController {
 		
 	}
 	
-		  
-	  
-	  @GetMapping("/auth/naver/callback") 
-	  public @ResponseBody UserVO callbackNaver(String code) {
-		  
-		  UserVO user = userService.naverUser(code);
-		  
-		  return user;
-		  }
+	
 }
--- _skylines/src/main/java/com/project/springboot/DAO/UserDAO.java
+++ _skylines/src/main/java/com/project/springboot/DAO/UserDAO.java
@@ -1,12 +1,16 @@
 package com.project.springboot.DAO;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.project.springboot.VO.UserVO;
 
 @Mapper
 public interface UserDAO {
-	UserVO selectOneId(String userId);
-	int simpleSign(UserVO user);
+	List<UserVO> selectAll();
+	UserVO inspectionId(String userId);
 	UserVO authentication(UserVO user);
+	int simpleSign(UserVO user);
+	int fullSign(UserVO user);
 }
