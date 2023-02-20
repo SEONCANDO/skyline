7b0e897761ee08b2d85fcf7b6859cb5fb0bb1a5c
dlqhdddd <118142998+dlqhdddd@users.noreply.github.com>
2023-02-20 21:53:47 +0900

    booking 노력중!!

--- _skylines/src/main/java/com/project/springboot/Controller/ReservationController.java
+++ _skylines/src/main/java/com/project/springboot/Controller/ReservationController.java
@@ -1,48 +1,55 @@
 package com.project.springboot.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.project.springboot.DAO.BbsDAO;
-import com.project.springboot.DAO.FlightScheduleDAO;
-import com.project.springboot.DAO.PlatformDAO;
 import com.project.springboot.DAO.ReservationDAO;
-import com.project.springboot.DAO.UserDAO;
-import com.project.springboot.Service.UserServiceImp;
 import com.project.springboot.VO.ReservationVO;
-import com.project.springboot.VO.UserVO;
 
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @Log4j2
-@RequiredArgsConstructor
 public class ReservationController {
 
 	@Autowired
-	UserServiceImp userService;
-	
-	PlatformDAO pfMapper;
-	FlightScheduleDAO fsMapper;
 	ReservationDAO rsMapper;
-	UserDAO userMapper;
-	BbsDAO bbsMapper;
-	
+
 	@Autowired
-	public ReservationController(PlatformDAO pfMapper, FlightScheduleDAO fsMapper, ReservationDAO rsMapper, UserDAO userMapper,BbsDAO bbsMapper) {
+	public ReservationController(ReservationDAO rsMapper) {
 		super();
-		this.pfMapper = pfMapper;
-		this.fsMapper = fsMapper;
+
 		this.rsMapper = rsMapper;
-		this.userMapper = userMapper;
-		this.bbsMapper = bbsMapper;
+
 	}
 	
+	@GetMapping("/rs_list")
+	public String list(Model model) {
+		List<ReservationVO> rs_vo = rsMapper.findAll();
+		model.addAttribute("rs_list",rs_vo);
+		return "rs_list";
+	}
 	
-
-
+	@PostMapping("/insert")
+	public String insert(ReservationVO rs_vo,Model model) {
+		//System.out.println(rs_vo);
+		int res = rsMapper.save(rs_vo);
+		//model.addAttribute("res",res);
+		//return "result";
+		return"redirect:/rs_list";
+		// redirect는 값을 못 넘김
+	}
+	
+	@GetMapping("/delete")
+	public String delete(int reservationNum) {
+		rsMapper.delete(reservationNum);
+		return "redirect:/rs_list";
+	}
+	
 }
+	
--- _skylines/src/main/java/com/project/springboot/DAO/ReservationDAO.java
+++ _skylines/src/main/java/com/project/springboot/DAO/ReservationDAO.java
@@ -3,10 +3,13 @@ package com.project.springboot.DAO;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+
 import com.project.springboot.VO.ReservationVO;
 
 @Mapper
 public interface ReservationDAO {
 	List<ReservationVO> selectAll();
+	List<ReservationVO> findAll();
+	int save (ReservationVO rs_vo);
+	int delete(int reservationNum);
 }
